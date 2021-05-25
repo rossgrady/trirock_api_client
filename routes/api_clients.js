bd4a342678bfdcf5a9f28085de3523aeec62bd9d
Ross Grady <ross@rossgrady.org>
2021-05-25 18:52:48 -0400

    complete refactor of the form objects

--- routes/api_clients.js
+++ routes/api_clients.js
@@ -474,12 +474,6 @@ async function events_add(bodyObj) {
   const returnarr = [];
   console.log(util.inspect(bodyObj, true, 8, true));
   for (const idx in bodyObj.activity_API_ID) {
-    console.log(idx);
-    console.log('am i losing my mind');
-    console.log(bodyObj.activity_venueID[idx]);
-    console.log(util.inspect(bodyObj.artistid[idx], true, 4, true));
-    console.log(util.inspect(bodyObj.addone[idx], true, 4, true));    
-    console.log(util.inspect(bodyObj.newartistname[idx], true, 4, true));
     const evtObj = {
       "activity_startDate": bodyObj.activity_startDate[idx],
       "activity_Time": bodyObj.activity_Time[idx],
--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/', async function(req, res, next) {
 });
 
 router.post('/events-add', async function(req, res, next) {
-  const events = await events_add(req.body);
+  console.log(util.inspect(req.body, true, 8, true));
   const renderObj = {
     title: 'First Pass!',
   }
