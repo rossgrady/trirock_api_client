5421536ae530abe1692f912380ed8e5d23887c40
Ross Grady <ross@rossgrady.org>
2021-05-29 17:03:28 -0400

    trying to see where the other half of them are going

--- routes/api_clients.js
+++ routes/api_clients.js
@@ -417,13 +417,14 @@ async function ical_events(baseURL, timeWindow, dbpool) {
     const webEvents = await ical.async.fromURL(venueURL);
     for (const idx in webEvents) {
       const startTime = dayjs(webEvents[idx].start);
-      console.log('starttime is ' + util.inspect(startTime, true, 3, true));
+      console.log('in ical -- starttime is ' + util.inspect(startTime, true, 3, true));
       if (webEvents[idx].type === 'VEVENT' && startTime.isAfter(dayjs())) {
         console.log('I *think* starttime is after now');
         //  && webEvents[idx].categories[0] === 'Show' -- not universal, sigh
         if (typeof webEvents[idx].categories !== 'undefined' && webEvents[idx].categories[0] !== 'undefined' && webEvents[idx].categories[0] !== 'Show') {
           continue;
         }
+        console.log('working on: ' + webEvents[idx].summary);
         const rawArtist = {
           "name": webEvents[idx].summary,
           "url": "",
@@ -496,7 +497,8 @@ async function tribe(baseURL, timeWindow, dbpool) {
       console.log('in tribe, about to lookup: ' + eventid);
       try {
         const eventdata = await axios.get(apiURL + eventid);
-        console.log('that lookup yielded: ' + util.inspect(eventdata.data, true, 1, true));
+        console.log('page parser produced: ' + title);
+        console.log('api produced: '+ eventdata.data.title);
         const rawArtist = {
           "name": title,
           "url": "",
@@ -529,6 +531,7 @@ async function tribe(baseURL, timeWindow, dbpool) {
             eventObj.activity_Blurb = artiste.blurb_snippet;
           }
         }
+        console.log(' about to push this to returnarr: ' + util.inspect(eventObj, true, 3, true));
         returnarr.push(eventObj);
       } catch (error) {
         console.error(error);
@@ -537,6 +540,7 @@ async function tribe(baseURL, timeWindow, dbpool) {
   } catch (error) {
     console.error(error);
   }
+  console.log('final return array from tribe: ' + util.inspect(returnarr, true, 4, true));
   return returnarr;
 }
 
