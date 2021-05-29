8a92d9239aa050aa457edc446de342133771e0c5
Ross Grady <ross@rossgrady.org>
2021-05-29 10:37:25 -0400

    well let's see

--- routes/api_clients.js
+++ routes/api_clients.js
@@ -463,7 +463,8 @@ async function tribe() {
   const l506url = 'https://local506.com/events/';
   const rawpage = await axios.get(l506url);
   const dompage = parser.parse(rawpage);
-  console.log(util.inspect(dompage, true, 9, true));
+  const targetdiv = dompage.querySelectorAll('.vcalendar');
+  console.log(util.inspect(targetdiv, true, 9, true));
 }
 
 async function main() {
