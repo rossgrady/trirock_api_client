2866c8b50d1afa5f7d34e368a94d322e31a29bab
Ross Grady <ross@rossgrady.org>
2021-07-31 15:12:13 -0400

    trying to debug the broken dedupe logic

--- routes/api_clients.js
+++ routes/api_clients.js
@@ -848,18 +848,36 @@ async function main() {
         if (main_events[venueid].events[`${evtday}`][0].activity_API_ID > main_events[venueid].events[`${evtday}`][1].activity_API_ID) {
           target_event = 0;
           source_event = 1;
+          if(venueid === '3') {
+            console.log("venue is Local 506 and I just swapped source and target");
+          }
         }
         for (const source_artist of main_events[venueid].events[`${evtday}`][source_event].artists) {
           let found = 0;
+          if(venueid === '3') {
+            console.log("comparing Local 506 artist " + source_artist);
+          }
           for (const target_artist of main_events[venueid].events[`${evtday}`][target_event].artists) {
+            if(venueid === '3') {
+              console.log("to " + target_artist);
+            }
             if (source_artist.origname === target_artist.origname) {
               found = 1;
             }
           }
           if (found === 0) {
+            if(venueid === '3') {
+              console.log("local 506 artist wasn't matched, so ");
+            }
             if (api_same === 0) {
+              if(venueid === '3') {
+                console.log("so pushing " + source_artist + " to target event");
+              }
               main_events[venueid].events[`${evtday}`][target_event].artists.push(source_artist);
             } else {
+              if(venueid === '3') {
+                console.log("setting identical to zero because . . . I'm not sure why actually");
+              }
               identical = 0;
             }
           }
@@ -875,9 +893,15 @@ async function main() {
           const removed = main_events[venueid].events[`${evtday}`].splice(source_event, 1);
           return_events.push(main_events[venueid].events[`${evtday}`][target_event]);
         } else if (api_same === 0) {
+          if(venueid === '3') {
+            console.log("different APIs so removing " + source_event);
+          }
           const removed = main_events[venueid].events[`${evtday}`].splice(source_event, 1);
           return_events.push(main_events[venueid].events[`${evtday}`][target_event]);
         } else {
+          if(venueid === '3') {
+            console.log("welp, just passing along both");
+          }
           return_events.push(main_events[venueid].events[`${evtday}`][target_event]);
           return_events.push(main_events[venueid].events[`${evtday}`][source_event]);
         }
